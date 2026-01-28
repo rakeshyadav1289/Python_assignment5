4ff0dbe85f9e92da59e0d60bbfa43265480e0ec1
rakeshyadav1289 <129735141+rakeshyadav1289@users.noreply.github.com>
2026-01-28 11:54:39 +0530

    Implement number extraction and reversal in task2.py

---
+++ task2.py
@@ -0,0 +1,12 @@
+# Create a list of numbers from 1 to 10
+numbers = list(range(1, 10))
+
+#Extract the first five elements
+extracted_list = numbers[:5]
+
+#Reverse the extracted elements
+reversed_list = extracted_list[::-1]
+
+#Printing extracted_list and reversed_list
+print("Extracted first 5 List:", extracted_list)
+print("Reversed extracted List:", reversed_list)
